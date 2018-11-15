db088e0641285034dd0b65a618065d260600f10d
gayathri <gayathrijayabalan96@gmail.com>
2018-11-15 13:14:30 +0530

    material install

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './coach/dashboard/dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AdminHeaderComponent } from './admin/admin-header/admin-header.component';
+import { LoginComponent } from './coach/login/login.component';
 
 const routes: Routes = [
  {path:'dashboard',component:DashboardComponent},
  {path:'navbar',component:NavbarComponent},
- {path:'admin-header',component:AdminHeaderComponent}
- 
+ {path:'admin-header',component:AdminHeaderComponent},
+ {path:'login',component:LoginComponent}
 ];
 
 @NgModule({
