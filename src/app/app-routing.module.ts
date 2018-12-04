ca41522adf31f5e2ebceec47c2943afa89cace97
abithakaruna <revathi.k2493@gmail.com>
2018-12-04 11:26:37 +0530

    student design changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { DashboardComponent } from './coach/dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AdminHeaderComponent } from './admin/admin-header/admin-header.component';
+import { AdminSidenavComponent } from './admin/admin-sidenav/admin-sidenav.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { ConditionComponent } from './coach/condition/condition/condition.component';
+import { LoginComponent } from './coach/login/login.component';
+import { EventsComponent } from './coach/events/events.component';
+import { AthleteComponent } from './admin/athlete/athlete.component';
+import { ReportsComponent } from './coach/reports/reports.component';
+import { HeaderComponent } from './coach/header/header.component';
+import { StudentComponent } from './coach/student/student.component';
+const routes: Routes = [
+ {path:'dashboard',component:DashboardComponent},
+ {path:'navbar',component:NavbarComponent},
+ {path:'admin-header',component:AdminHeaderComponent},
+ {path:'admin-sidenav',component:AdminSidenavComponent},
+ {path:'admin-dashboard',component:AdminDashboardComponent},
+ {path:'condition',component:ConditionComponent},
+ {path:'login',component:LoginComponent},
+ {path:'athlete',component:AthleteComponent},
+ {path:'reports',component:ReportsComponent},
+ {path:'events',component:EventsComponent},
+ {path:'header',component:HeaderComponent},
+ {path:'student',component:StudentComponent} 
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/coach/login/login.component.ts
+++ src/app/coach/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  model: any={};
+  constructor(){}
 
   ngOnInit() {
-  }
 
+}
 }
