6b22ced2d4bc4cf449772ae516388fc7d94d791b
gayathri <gayathrijayabalan96@gmail.com>
2018-12-31 13:16:39 +0530

    exercise

--- src/app/shared/user.model.ts
+++ src/app/shared/user.model.ts
@@ -16,16 +16,16 @@ export interface User {
 export interface Addschedule{
     id:string;
     Name:string;
-    date:string;
-    distance:string;
-    tag1:string;
-    tag2:string;
-    tag3:string;
-    tag4:string;
-    tag5:string;
-    tag6:string;
-    tag7:string;
-    tag8:string;
-    tag9:string;
-    tag10:string;
+    Date:string;
+    Distance:string;
+    Tag1:string;
+    Tag2:string;
+    Tag3:string;
+    Tag4:string;
+    Tag5:string;
+    Tag6:string;
+    Tag7:string;
+    Tag8:string;
+    Tag9:string;
+    Tag10:string;
 }
