8b308cbe6e8f74dc5513340b50debcf2eb97b156
sindhupalanivel <sindhuthaman6@gmail.com>
2019-01-09 15:06:30 +0530

    profile image

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../shared/auth.service';
+import{AngularFireAuth} from 'angularfire2/auth';
+import{Router} from '@angular/router';
+import * as firebase from 'firebase/app';
+
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
