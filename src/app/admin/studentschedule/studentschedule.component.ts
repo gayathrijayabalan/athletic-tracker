8be002021e8a89c62788d2d767c08e9dbc565df8
gayathri <gayathrijayabalan96@gmail.com>
2019-02-13 15:12:41 +0530

    admin header final

--- src/app/admin/studentschedule/studentschedule.component.ts
+++ src/app/admin/studentschedule/studentschedule.component.ts
@@ -4,25 +4,28 @@ import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
 import { CoachserviceService } from '../../shared/coachservice.service';
 import { NgForm } from '@angular/forms';
+import { User} from '../../shared/user.model';
 @Component({
   selector: 'app-studentschedule',
   templateUrl: './studentschedule.component.html',
   styleUrls: ['./studentschedule.component.css']
 })
 export class StudentscheduleComponent implements OnInit {
-
+  list:User[];
+  user:any;
   constructor(public service: CoachserviceService,private afs :AngularFirestore) { }
 
   ngOnInit() {
     this.resetForm();
+    
   }
   resetForm(form?: NgForm) {
     if (form !=null)
     form.resetForm();
     this.service.formworkout= {
       id: null,
+      Athlete1: '',
       Date: '',
-      Name: '',
       Workout1:'',
       Workout2:'', 
       Workout3:'',
@@ -30,8 +33,11 @@ export class StudentscheduleComponent implements OnInit {
     }
   }
   onSubmit(form:NgForm){
+    
     let data =form.value;
     this.afs.collection('workout').add(data);
     this.resetForm(form);
+    console.log("df");
   }
+
 }
